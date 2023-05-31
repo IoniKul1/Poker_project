9d0590024a712d7891a60c18c1dd3ec3f3c3df8f
IoniKul1 <ykullock@mail.utdt.edu>
2023-05-31 19:37:00 -0300

    up

--- index.py
+++ index.py
@@ -1,40 +1,44 @@
 cartas_validas = ['As', 'Ah', 'Ad', 'Ac',
-                      'Ks', 'Kh', 'Kd', 'Kc',
-                      'Qs', 'Qh', 'Qd', 'Qc',
-                      'Js', 'Jh', 'Jd', 'Jc',
-                      'Ts', 'Th', 'Td', 'Tc',
-                      '9s', '9h', '9d', '9c',
-                      '8s', '8h', '8d', '8c',
-                      '7s', '7h', '7d', '7c',
-                      '6s', '6h', '6d', '6c',
-                      '5s', '5h', '5d', '5c',
-                      '4s', '4h', '4d', '4c',
-                      '3s', '3h', '3d', '3c',
-                      '2s', '2h', '2d', '2c']
+                  'Ks', 'Kh', 'Kd', 'Kc',
+                  'Qs', 'Qh', 'Qd', 'Qc',
+                  'Js', 'Jh', 'Jd', 'Jc',
+                  'Ts', 'Th', 'Td', 'Tc',
+                  '9s', '9h', '9d', '9c',
+                  '8s', '8h', '8d', '8c',
+                  '7s', '7h', '7d', '7c',
+                  '6s', '6h', '6d', '6c',
+                  '5s', '5h', '5d', '5c',
+                  '4s', '4h', '4d', '4c',
+                  '3s', '3h', '3d', '3c',
+                  '2s', '2h', '2d', '2c']
 
 
 def mi_funcion():
-    # Ingresar los parámetros mediante input
-    cartas = str(input("Ingrese las cartas (de mayor a menor): "))
-    suited = bool(input("¿Las cartas son del mismo palo? (True/False): "))
-    stack_ft = int(input("Ingrese el valor de stack_ft: "))
-    posicion = int(input("Ingrese el valor de posición: "))
-    rivales = int(input("Ingrese el valor de los rivales: "))
-    fish =  bool(input("¿Las cartas son del mismo palo? (True/False): "))
-
-    #if (cartas in cartas_validas):
-        #if(rivales = 1):
-         #   posicion_rival = str(input("Que posición tiene tu rival: "))#bb= big blind, sb= small blind, btn= button
-          #      if(posicion_rival == "bb"):
-                    
-           #     if(posicion_rival == "sb"):
-                
-            #    if(posicion_rival == "btn"):
-                
-        #if(rivales = 2)
     
+    cartas = list(input("Ingrese las cartas (de mayor a menor, separadas por un espacio): ").split())
+    stack_ft = int(input("Ingrese el valor de stack_ft: "))
+    posicion = str(input("¿Qué posición tienes (bb/sb/btn): "))
+    rivales = int(input("Ingrese cantida rivales: "))
+    fish = bool(input("¿Es pro? (True/False): "))
+
+    if cartas[0][1] == cartas[1][1]:
+        suited = True
+
+
+    if cartas[0] and cartas[1] in cartas_validas:
+        if rivales == 1:
+            posicion_rival = str(input("¿Qué posición tiene tu rival? (bb/sb/btn): "))
+            if posicion_rival == "bb":
+                print("call")
+            elif posicion_rival == "sb":
+                print("fold")
+            elif posicion_rival == "btn":
+                print("fold")
+        elif rivales == 2:
+            print("call")
+
     # Ejemplo de devolución de resultados
-    return cartas, suited, stack_ft, posicion, rivales
+    return cartas, suited, stack_ft, posicion, rivales, fish
 
 # Llamar a la función para probarla
 resultado = mi_funcion()
