cf89f992a2dd3c48e8fcd6201b632bb21249c57f
Yoni Kullock <101648067+IoniKul1@users.noreply.github.com>
2023-05-31 20:19:15 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
+        #clubs (♧), diamonds (♢), hearts (♥) and spades (♤)
 cartas_validas = ['As', 'Ah', 'Ad', 'Ac',
                   'Ks', 'Kh', 'Kd', 'Kc',
                   'Qs', 'Qh', 'Qd', 'Qc',
                   'Js', 'Jh', 'Jd', 'Jc',
-                  'Ts', 'Th', 'Td', 'Tc',
                   '9s', '9h', '9d', '9c',
                   '8s', '8h', '8d', '8c',
                   '7s', '7h', '7d', '7c',
@@ -12,13 +12,12 @@ cartas_validas = ['As', 'Ah', 'Ad', 'Ac',
                   '3s', '3h', '3d', '3c',
                   '2s', '2h', '2d', '2c']
 
-
 def mi_funcion():
     
     cartas = list(input("Ingrese las cartas (de mayor a menor, separadas por un espacio): ").split())
     stack_ft = int(input("Ingrese el valor de stack_ft: "))
     posicion = str(input("¿Qué posición tienes (bb/sb/btn): "))
-    rivales = int(input("Ingrese cantida rivales: "))
+    rivales = int(input("Ingrese cantidad de rivales: "))
     fish = bool(input("¿Es pro? (True/False): "))
 
     if cartas[0][1] == cartas[1][1]:
@@ -34,6 +33,7 @@ def mi_funcion():
                 print("fold")
             elif posicion_rival == "btn":
                 print("fold")
+
         elif rivales == 2:
             print("call")
 
