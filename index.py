2ff31ae351d3aa8cbb92fe0fa19bba47119ed496
IoniKul1 <ykullock@mail.utdt.edu>
2023-05-31 20:52:56 -0300

    first table

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ cartas_validas = ['As', 'Ah', 'Ad', 'Ac',
                   'Ks', 'Kh', 'Kd', 'Kc',
                   'Qs', 'Qh', 'Qd', 'Qc',
                   'Js', 'Jh', 'Jd', 'Jc',
+                  'Ts', 'Th', 'Td', 'Tc',
                   '9s', '9h', '9d', '9c',
                   '8s', '8h', '8d', '8c',
                   '7s', '7h', '7d', '7c',
@@ -13,7 +14,8 @@ cartas_validas = ['As', 'Ah', 'Ad', 'Ac',
                   '2s', '2h', '2d', '2c']
 
 def mi_funcion():
-    
+
+    bb = int(input("ingrese su cantidad de bb: "))
     cartas = list(input("Ingrese las cartas (de mayor a menor, separadas por un espacio): ").split())
     stack_ft = int(input("Ingrese el valor de stack_ft: "))
     posicion = str(input("¿Qué posición tienes (bb/sb/btn): "))
@@ -23,12 +25,38 @@ def mi_funcion():
     if cartas[0][1] == cartas[1][1]:
         suited = True
 
-
     if cartas[0] and cartas[1] in cartas_validas:
         if rivales == 1:
             posicion_rival = str(input("¿Qué posición tiene tu rival? (bb/sb/btn): "))
-            if posicion_rival == "bb":
-                print("call")
+            if posicion_rival == "bb" and posicion == "sb":
+                if bb in [20,21,22,23,24,25]:
+                    if cartas[0][0] == "A" and cartas[1][0] == "A":
+                        print("Raise 3x")
+                    elif cartas[0][0] == "A" and cartas[1][0] == "K":
+                        print("Raise 3x")
+                    elif cartas[0][0] == "A" and cartas[1][0] == "Q":
+                        print("Raise 3x")
+                    elif cartas[0][0] == "A" and cartas[1][0] == "J":
+                        print("Raise 3x")
+                    elif cartas[0][0] == "A" and cartas[1][0] == "T" and suited==True:
+                        print("Raise 3x")
+                    elif cartas[0][0] == "A" and cartas[1][0] == "9" and suited==True:
+                        print("Raise 3x")
+                    elif cartas[0][0] == "A" and cartas[1][0] == "8" and suited==True:
+                        print("Raise 3x")
+                    
+                    elif cartas[0][0] == "K" and cartas[1][0] == "K":
+                        print("Raise 3x")
+                    elif cartas[0][0] == "K" and cartas[1][0] == "Q":
+                        print("Raise 3x")
+                    elif cartas[0][0] == "K" and cartas[1][0] == "J":
+                        print("Raise 3x")
+
+                        
+                    elif cartas[0][0] == "Q" and cartas[1][0] == "Q":
+                        print("Raise 3x")
+
+
             elif posicion_rival == "sb":
                 print("fold")
             elif posicion_rival == "btn":
